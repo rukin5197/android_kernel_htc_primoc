42af7848cf68174e16bc0f08f8157f542dd93a5c
James Taylor <jmztaylor@gmail.com>
2012-10-24 18:23:16 -0400

    fix backlight  -- Thanks to simonsimons34

--- arch/arm/mach-msm/display/primoc-panel.c
+++ arch/arm/mach-msm/display/primoc-panel.c
@@ -200,6 +200,9 @@ primoc_backlight_switch(int on)
 	} else {
 		clear_bit(GATE_ON, &cabc.status);
 		cabc.last_shrink_br = 0;
+                printk(KERN_DEBUG "turn off backlight\n");
+		max8957_set_bl_on_off(MAX8957_WLED_1, MAX8957_WLED_DIS);
+		primoc_set_brightness(&cabc.lcd_backlight, 0);
 	}
 }
 
